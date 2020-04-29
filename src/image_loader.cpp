fbb45a605e174957c8b35ea3bb105bf039551be3
alvarovillena <avillena@robotnik.es>
2020-04-29 13:07:04 +0200

    Clang formatting

--- src/image_loader.cpp
+++ src/image_loader.cpp
@@ -52,12 +52,8 @@
 
 namespace multimap_server
 {
-
-void
-loadMapFromFile(nav_msgs::GetMap::Response* resp,
-                const char* fname, double res, bool negate,
-                double occ_th, double free_th, double* origin,
-                MapMode mode)
+void loadMapFromFile(nav_msgs::GetMap::Response* resp, const char* fname, double res, bool negate, double occ_th,
+                     double free_th, double* origin, MapMode mode)
 {
   SDL_Surface* img;
 
@@ -65,7 +61,7 @@ loadMapFromFile(nav_msgs::GetMap::Response* resp,
   unsigned char* p;
   unsigned char value;
   int rowstride, n_channels, avg_channels;
-  unsigned int i,j;
+  unsigned int i, j;
   int k;
   double occ;
   int alpha;
@@ -73,10 +69,10 @@ loadMapFromFile(nav_msgs::GetMap::Response* resp,
   double color_avg;
 
   // Load the image using SDL.  If we get NULL back, the image load failed.
-  if(!(img = IMG_Load(fname)))
+  if (!(img = IMG_Load(fname)))
   {
-    std::string errmsg = std::string("failed to open image file \"") +
-            std::string(fname) + std::string("\": ") + IMG_GetError();
+    std::string errmsg =
+        std::string("failed to open image file \"") + std::string(fname) + std::string("\": ") + IMG_GetError();
     throw std::runtime_error(errmsg);
   }
 
@@ -85,11 +81,11 @@ loadMapFromFile(nav_msgs::GetMap::Response* resp,
   resp->map.info.height = img->h;
   resp->map.info.resolution = res;
   resp->map.info.origin.position.x = *(origin);
-  resp->map.info.origin.position.y = *(origin+1);
+  resp->map.info.origin.position.y = *(origin + 1);
   resp->map.info.origin.position.z = 0.0;
   btQuaternion q;
   // setEulerZYX(yaw, pitch, roll)
-  q.setEulerZYX(*(origin+2), 0, 0);
+  q.setEulerZYX(*(origin + 2), 0, 0);
   resp->map.info.origin.orientation.x = q.x();
   resp->map.info.origin.orientation.y = q.y();
   resp->map.info.origin.orientation.z = q.z();
@@ -104,38 +100,38 @@ loadMapFromFile(nav_msgs::GetMap::Response* resp,
 
   // NOTE: Trinary mode still overrides here to preserve existing behavior.
   // Alpha will be averaged in with color channels when using trinary mode.
-  if (mode==TRINARY || !img->format->Amask)
+  if (mode == TRINARY || !img->format->Amask)
     avg_channels = n_channels;
   else
     avg_channels = n_channels - 1;
 
   // Copy pixel data into the map structure
   pixels = (unsigned char*)(img->pixels);
-  for(j = 0; j < resp->map.info.height; j++)
+  for (j = 0; j < resp->map.info.height; j++)
   {
     for (i = 0; i < resp->map.info.width; i++)
     {
       // Compute mean of RGB for this pixel
-      p = pixels + j*rowstride + i*n_channels;
+      p = pixels + j * rowstride + i * n_channels;
       color_sum = 0;
-      for(k=0;k<avg_channels;k++)
+      for (k = 0; k < avg_channels; k++)
         color_sum += *(p + (k));
       color_avg = color_sum / (double)avg_channels;
 
       if (n_channels == 1)
-          alpha = 1;
+        alpha = 1;
       else
-          alpha = *(p+n_channels-1);
+        alpha = *(p + n_channels - 1);
 
-      if(negate)
+      if (negate)
         color_avg = 255 - color_avg;
 
-      if(mode==RAW){
-          value = color_avg;
-          resp->map.data[MAP_IDX(resp->map.info.width,i,resp->map.info.height - j - 1)] = value;
-          continue;
+      if (mode == RAW)
+      {
+        value = color_avg;
+        resp->map.data[MAP_IDX(resp->map.info.width, i, resp->map.info.height - j - 1)] = value;
+        continue;
       }
-
 
       // If negate is true, we consider blacker pixels free, and whiter
       // pixels free.  Otherwise, it's vice versa.
@@ -144,22 +140,22 @@ loadMapFromFile(nav_msgs::GetMap::Response* resp,
       // Apply thresholds to RGB means to determine occupancy values for
       // map.  Note that we invert the graphics-ordering of the pixels to
       // produce a map with cell (0,0) in the lower-left corner.
-      if(occ > occ_th)
+      if (occ > occ_th)
         value = +100;
-      else if(occ < free_th)
+      else if (occ < free_th)
         value = 0;
-      else if(mode==TRINARY || alpha < 1.0)
+      else if (mode == TRINARY || alpha < 1.0)
         value = -1;
-      else {
+      else
+      {
         double ratio = (occ - free_th) / (occ_th - free_th);
         value = 99 * ratio;
       }
 
-      resp->map.data[MAP_IDX(resp->map.info.width,i,resp->map.info.height - j - 1)] = value;
+      resp->map.data[MAP_IDX(resp->map.info.width, i, resp->map.info.height - j - 1)] = value;
     }
   }
 
   SDL_FreeSurface(img);
 }
-
 }
--- src/main.cpp
+++ src/main.cpp
@@ -29,8 +29,9 @@
 
 /* Author: Brian Gerkey */
 
-#define USAGE "\nUSAGE: multimap_server <multimap_server_config.yaml>\n" \
-              "  multimap_server_config.yaml: Indicates which maps are going to be loaded and info on how to do it"
+#define USAGE                                                                                                          \
+  "\nUSAGE: multimap_server <multimap_server_config.yaml>\n"                                                           \
+  "  multimap_server_config.yaml: Indicates which maps are going to be loaded and info on how to do it"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -46,12 +47,11 @@
 #include <multimap_server_msgs/LoadMap.h>
 #include <ros/package.h>
 
-
 #ifdef HAVE_YAMLCPP_GT_0_5_0
 // The >> operator disappeared in yaml-cpp 0.5, so this function is
 // added to provide support for code written under the yaml-cpp 0.3 API.
-template<typename T>
-void operator >> (const YAML::Node& node, T& i)
+template <typename T>
+void operator>>(const YAML::Node& node, T& i)
 {
   i = node.as<T>();
 }
@@ -59,285 +59,303 @@ void operator >> (const YAML::Node& node, T& i)
 
 class Map
 {
-  public:
+public:
+  std::string map_fullname;
 
-    std::string map_fullname;
+  Map(const std::string& fname, const std::string& ns, const std::string& desired_name,
+      const std::string& global_frame_id)
+  {
+    std::string mapfname = "";
+    double origin[3];
+    int negate;
+    double occ_th, free_th;
+    MapMode mode = TRINARY;
+    double resolution;
 
-    Map(const std::string& fname, const std::string& ns, const std::string& desired_name, const std::string& global_frame_id)
+    map_fullname = ns + "/" + desired_name;
+
+    std::ifstream fin(fname.c_str());
+    if (fin.fail())
     {
-      std::string mapfname = "";
-      double origin[3];
-      int negate;
-      double occ_th, free_th;
-      MapMode mode = TRINARY;
-      double resolution;
+      ROS_ERROR("Multimap_server could not open %s.", fname.c_str());
+      exit(-1);
+    }
+#ifdef HAVE_YAMLCPP_GT_0_5_0
+    // The document loading process changed in yaml-cpp 0.5.
+    YAML::Node doc = YAML::Load(fin);
+#else
+    YAML::Parser parser(fin);
+    YAML::Node doc;
+    parser.GetNextDocument(doc);
+#endif
 
-      map_fullname = ns + "/" + desired_name;
+    try
+    {
+      doc["resolution"] >> resolution;
+    }
+    catch (YAML::InvalidScalar)
+    {
+      ROS_ERROR("The map does not contain a resolution tag or it is invalid.");
+      exit(-1);
+    }
+    try
+    {
+      doc["negate"] >> negate;
+    }
+    catch (YAML::InvalidScalar)
+    {
+      ROS_ERROR("The map does not contain a negate tag or it is invalid.");
+      exit(-1);
+    }
+    try
+    {
+      doc["occupied_thresh"] >> occ_th;
+    }
+    catch (YAML::InvalidScalar)
+    {
+      ROS_ERROR("The map does not contain an occupied_thresh tag or it is invalid.");
+      exit(-1);
+    }
+    try
+    {
+      doc["free_thresh"] >> free_th;
+    }
+    catch (YAML::InvalidScalar)
+    {
+      ROS_ERROR("The map does not contain a free_thresh tag or it is invalid.");
+      exit(-1);
+    }
+    try
+    {
+      std::string modeS = "";
+      doc["mode"] >> modeS;
 
-      std::ifstream fin(fname.c_str());
-      if (fin.fail()) {
-        ROS_ERROR("Multimap_server could not open %s.", fname.c_str());
-        exit(-1);
-      }
-  #ifdef HAVE_YAMLCPP_GT_0_5_0
-      // The document loading process changed in yaml-cpp 0.5.
-      YAML::Node doc = YAML::Load(fin);
-  #else
-      YAML::Parser parser(fin);
-      YAML::Node doc;
-      parser.GetNextDocument(doc);
-  #endif
-
-      try {
-        doc["resolution"] >> resolution;
-      } catch (YAML::InvalidScalar) {
-        ROS_ERROR("The map does not contain a resolution tag or it is invalid.");
-        exit(-1);
-      }
-      try {
-        doc["negate"] >> negate;
-      } catch (YAML::InvalidScalar) {
-        ROS_ERROR("The map does not contain a negate tag or it is invalid.");
-        exit(-1);
-      }
-      try {
-        doc["occupied_thresh"] >> occ_th;
-      } catch (YAML::InvalidScalar) {
-        ROS_ERROR("The map does not contain an occupied_thresh tag or it is invalid.");
-        exit(-1);
-      }
-      try {
-        doc["free_thresh"] >> free_th;
-      } catch (YAML::InvalidScalar) {
-        ROS_ERROR("The map does not contain a free_thresh tag or it is invalid.");
-        exit(-1);
-      }
-      try {
-        std::string modeS = "";
-        doc["mode"] >> modeS;
-
-        if(modeS=="trinary")
-          mode = TRINARY;
-        else if(modeS=="scale")
-          mode = SCALE;
-        else if(modeS=="raw")
-          mode = RAW;
-        else{
-          ROS_ERROR("Invalid mode tag \"%s\".", modeS.c_str());
-          exit(-1);
-        }
-      } catch (YAML::Exception) {
-        ROS_DEBUG("The map does not contain a mode tag or it is invalid... assuming Trinary");
+      if (modeS == "trinary")
         mode = TRINARY;
-      }
-      try {
-        doc["origin"][0] >> origin[0];
-        doc["origin"][1] >> origin[1];
-        doc["origin"][2] >> origin[2];
-      } catch (YAML::InvalidScalar) {
-        ROS_ERROR("The map does not contain an origin tag or it is invalid.");
+      else if (modeS == "scale")
+        mode = SCALE;
+      else if (modeS == "raw")
+        mode = RAW;
+      else
+      {
+        ROS_ERROR("Invalid mode tag \"%s\".", modeS.c_str());
         exit(-1);
       }
-      try {
-        doc["image"] >> mapfname;
-        // TODO: make this path-handling more robust
-        if(mapfname.size() == 0)
-        {
-          ROS_ERROR("The image tag cannot be an empty string.");
-          exit(-1);
-        }
-        if(mapfname[0] != '/')
-        {
-          // dirname can modify what you pass it
-          char* fname_copy = strdup(fname.c_str());
-          mapfname = std::string(dirname(fname_copy)) + '/' + mapfname;
-          free(fname_copy);
-        }
-      } catch (YAML::InvalidScalar) {
-        ROS_ERROR("The map does not contain an image tag or it is invalid.");
+    }
+    catch (YAML::Exception)
+    {
+      ROS_DEBUG("The map does not contain a mode tag or it is invalid... assuming Trinary");
+      mode = TRINARY;
+    }
+    try
+    {
+      doc["origin"][0] >> origin[0];
+      doc["origin"][1] >> origin[1];
+      doc["origin"][2] >> origin[2];
+    }
+    catch (YAML::InvalidScalar)
+    {
+      ROS_ERROR("The map does not contain an origin tag or it is invalid.");
+      exit(-1);
+    }
+    try
+    {
+      doc["image"] >> mapfname;
+      // TODO: make this path-handling more robust
+      if (mapfname.size() == 0)
+      {
+        ROS_ERROR("The image tag cannot be an empty string.");
         exit(-1);
       }
-
-      ROS_INFO("Loading map from image \"%s\"", mapfname.c_str());
-      try
+      if (mapfname[0] != '/')
       {
-          multimap_server::loadMapFromFile(&map_resp_,mapfname.c_str(),resolution,negate,occ_th,free_th, origin, mode);
+        // dirname can modify what you pass it
+        char* fname_copy = strdup(fname.c_str());
+        mapfname = std::string(dirname(fname_copy)) + '/' + mapfname;
+        free(fname_copy);
       }
-      catch (std::runtime_error e)
-      {
-          ROS_ERROR("%s", e.what());
-          exit(-1);
-      }
-      // To make sure get a consistent time in simulation
-      ros::Time::waitForValid();
-      map_resp_.map.info.map_load_time = ros::Time::now();
-      map_resp_.map.header.frame_id = global_frame_id;
-      map_resp_.map.header.stamp = ros::Time::now();
-      ROS_INFO("Read a %d X %d map @ %.3lf m/cell",
-               map_resp_.map.info.width,
-               map_resp_.map.info.height,
-               map_resp_.map.info.resolution);
-      meta_data_message_ = map_resp_.map.info;
-
-      std::string service_name = ns + "/" + desired_name + "/" + "static_map";
-      service = n.advertiseService(service_name, &Map::mapCallback, this);
-
-      // Latched publisher for metadata
-      std::string metadata_topic_name = ns + "/" + desired_name + "/" + "map_metadata";
-      metadata_pub= n.advertise<nav_msgs::MapMetaData>(metadata_topic_name, 1, true);
-      metadata_pub.publish( meta_data_message_ );
-
-      // Latched publisher for data
-      std::string map_topic_name = ns + "/" + desired_name + "/" + "map";
-      map_pub = n.advertise<nav_msgs::OccupancyGrid>(map_topic_name, 1, true);
-      map_pub.publish( map_resp_.map );
     }
-
-    std::string getMapFullName()
+    catch (YAML::InvalidScalar)
     {
-      return map_fullname;
+      ROS_ERROR("The map does not contain an image tag or it is invalid.");
+      exit(-1);
     }
 
-  private:
-    ros::NodeHandle n;
-    ros::Publisher map_pub;
-    ros::Publisher metadata_pub;
-    ros::ServiceServer service;
-
-    /** Callback invoked when someone requests our service */
-    bool mapCallback(nav_msgs::GetMap::Request  &req,
-                     nav_msgs::GetMap::Response &res )
+    ROS_INFO("Loading map from image \"%s\"", mapfname.c_str());
+    try
     {
-      // request is empty; we ignore it
-
-      // = operator is overloaded to make deep copy (tricky!)
-      res = map_resp_;
-      ROS_INFO("Sending map");
-
-      return true;
+      multimap_server::loadMapFromFile(&map_resp_, mapfname.c_str(), resolution, negate, occ_th, free_th, origin, mode);
     }
+    catch (std::runtime_error e)
+    {
+      ROS_ERROR("%s", e.what());
+      exit(-1);
+    }
+    // To make sure get a consistent time in simulation
+    ros::Time::waitForValid();
+    map_resp_.map.info.map_load_time = ros::Time::now();
+    map_resp_.map.header.frame_id = global_frame_id;
+    map_resp_.map.header.stamp = ros::Time::now();
+    ROS_INFO("Read a %d X %d map @ %.3lf m/cell", map_resp_.map.info.width, map_resp_.map.info.height,
+             map_resp_.map.info.resolution);
+    meta_data_message_ = map_resp_.map.info;
 
-    /** The map data is cached here, to be sent out to service callers
-     */
-    nav_msgs::MapMetaData meta_data_message_;
-    nav_msgs::GetMap::Response map_resp_;
+    std::string service_name = ns + "/" + desired_name + "/" + "static_map";
+    service = n.advertiseService(service_name, &Map::mapCallback, this);
 
+    // Latched publisher for metadata
+    std::string metadata_topic_name = ns + "/" + desired_name + "/" + "map_metadata";
+    metadata_pub = n.advertise<nav_msgs::MapMetaData>(metadata_topic_name, 1, true);
+    metadata_pub.publish(meta_data_message_);
+
+    // Latched publisher for data
+    std::string map_topic_name = ns + "/" + desired_name + "/" + "map";
+    map_pub = n.advertise<nav_msgs::OccupancyGrid>(map_topic_name, 1, true);
+    map_pub.publish(map_resp_.map);
+  }
+
+  std::string getMapFullName()
+  {
+    return map_fullname;
+  }
+
+private:
+  ros::NodeHandle n;
+  ros::Publisher map_pub;
+  ros::Publisher metadata_pub;
+  ros::ServiceServer service;
+
+  /** Callback invoked when someone requests our service */
+  bool mapCallback(nav_msgs::GetMap::Request& req, nav_msgs::GetMap::Response& res)
+  {
+    // request is empty; we ignore it
+
+    // = operator is overloaded to make deep copy (tricky!)
+    res = map_resp_;
+    ROS_INFO("Sending map");
+
+    return true;
+  }
+
+  /** The map data is cached here, to be sent out to service callers
+   */
+  nav_msgs::MapMetaData meta_data_message_;
+  nav_msgs::GetMap::Response map_resp_;
 };
-
 
 class MultimapServer
 {
-  public:
-    /** Trivial constructor */
-    MultimapServer(const std::string& fname)
-    {
-      std::string load_map_service_name = "load_map";
-      load_map_service = n.advertiseService(load_map_service_name, &MultimapServer::loadMapCallback, this);
+public:
+  /** Trivial constructor */
+  MultimapServer(const std::string& fname)
+  {
+    std::string load_map_service_name = "load_map";
+    load_map_service = n.advertiseService(load_map_service_name, &MultimapServer::loadMapCallback, this);
 
-      std::ifstream fin(fname.c_str());
-      if (fin.fail()) {
-        ROS_ERROR("Multimap_server could not open %s.", fname.c_str());
-        exit(-1);
-      }
+    std::ifstream fin(fname.c_str());
+    if (fin.fail())
+    {
+      ROS_ERROR("Multimap_server could not open %s.", fname.c_str());
+      exit(-1);
+    }
 #ifdef HAVE_YAMLCPP_GT_0_5_0
-      // The document loading process changed in yaml-cpp 0.5.
-      YAML::Node doc = YAML::Load(fin);
+    // The document loading process changed in yaml-cpp 0.5.
+    YAML::Node doc = YAML::Load(fin);
 #else
-      YAML::Parser parser(fin);
-      YAML::Node doc;
-      parser.GetNextDocument(doc);
+    YAML::Parser parser(fin);
+    YAML::Node doc;
+    parser.GetNextDocument(doc);
 #endif
 
-      for (YAML::const_iterator namespace_iterator = doc.begin(); namespace_iterator != doc.end(); ++namespace_iterator)
+    for (YAML::const_iterator namespace_iterator = doc.begin(); namespace_iterator != doc.end(); ++namespace_iterator)
+    {
+      std::string global_frame = namespace_iterator->second["global_frame"].as<std::string>();
+      YAML::Node maps = namespace_iterator->second["maps"];
+      for (YAML::const_iterator maps_iterator = maps.begin(); maps_iterator != maps.end(); ++maps_iterator)
       {
-        std::string global_frame = namespace_iterator->second["global_frame"].as<std::string>();
-        YAML::Node maps = namespace_iterator->second["maps"];
-        for (YAML::const_iterator maps_iterator = maps.begin(); maps_iterator != maps.end(); ++maps_iterator)
-        {
-          std::string map_path = ros::package::getPath(namespace_iterator->second["maps_package"].as<std::string>()) +
-            "/" + maps_iterator->second.as<std::string>();
-          std::string map_namespace = namespace_iterator->first.as<std::string>();
-          std::string map_name = maps_iterator->first.as<std::string>();
-          std::string map_frame = namespace_iterator->second["global_frame"].as<std::string>();
+        std::string map_path = ros::package::getPath(namespace_iterator->second["maps_package"].as<std::string>()) +
+                               "/" + maps_iterator->second.as<std::string>();
+        std::string map_namespace = namespace_iterator->first.as<std::string>();
+        std::string map_name = maps_iterator->first.as<std::string>();
+        std::string map_frame = namespace_iterator->second["global_frame"].as<std::string>();
 
-          if(isMapAlreadyLoaded(map_namespace, map_name) == true)
+        if (isMapAlreadyLoaded(map_namespace, map_name) == true)
+        {
+          ROS_WARN("A map with the name %s/%s is already loaded", map_namespace.c_str(), map_name.c_str());
+        }
+        else
+        {
+          try
           {
-            ROS_WARN("A map with the name %s/%s is already loaded", map_namespace.c_str(), map_name.c_str());
+            Map* new_map = new Map(map_path, map_namespace, map_name, map_frame);
+            maps_vector.push_back(*new_map);
           }
-          else
+          catch (std::exception& e)
           {
-            try
-            {
-              Map *new_map = new Map(map_path, map_namespace, map_name, map_frame);
-              maps_vector.push_back(*new_map);
-            }
-            catch(std::exception& e) {
-              ROS_WARN("load_map service failed with exception: %s", e.what());
-            }
+            ROS_WARN("load_map service failed with exception: %s", e.what());
           }
         }
       }
     }
+  }
 
-  private:
-    ros::NodeHandle n;
-    ros::ServiceServer load_map_service;
-    ros::ServiceServer dump_map_service;
-    std::vector<Map> maps_vector;
+private:
+  ros::NodeHandle n;
+  ros::ServiceServer load_map_service;
+  ros::ServiceServer dump_map_service;
+  std::vector<Map> maps_vector;
 
-    bool loadMapCallback(multimap_server_msgs::LoadMap::Request &req,
-                          multimap_server_msgs::LoadMap::Response &res)
+  bool loadMapCallback(multimap_server_msgs::LoadMap::Request& req, multimap_server_msgs::LoadMap::Response& res)
+  {
+    if (isMapAlreadyLoaded(req.ns, req.map_name) == true)
     {
-      if(isMapAlreadyLoaded(req.ns, req.map_name) == true)
-      {
-        res.success = false;
-        res.msg = "load_map service failed: a map with the same name is already loaded";
-        return true;
-      }
-
-      try
-      {
-        Map *new_map = new Map(req.map_url, req.ns, req.map_name, req.global_frame);
-        maps_vector.push_back(*new_map);
-      }
-      catch(std::exception& e) {
-        res.success = false;
-        res.msg = "load_map service failed with exception: " + std::string(e.what());
-        return true;
-      }
-
-      res.success = true;
-      res.msg = "load_map service worked succesfully for: " + std::string(req.map_url);
+      res.success = false;
+      res.msg = "load_map service failed: a map with the same name is already loaded";
       return true;
     }
 
-    bool dumpMapCallback(multimap_server_msgs::LoadMap::Request &req,
-                          multimap_server_msgs::LoadMap::Response &res)
+    try
     {
-      // TODO
+      Map* new_map = new Map(req.map_url, req.ns, req.map_name, req.global_frame);
+      maps_vector.push_back(*new_map);
+    }
+    catch (std::exception& e)
+    {
+      res.success = false;
+      res.msg = "load_map service failed with exception: " + std::string(e.what());
       return true;
     }
 
-    bool isMapAlreadyLoaded(std::string ns, std::string map_name)
-    {
-      std::string map_fullname = ns + "/" + map_name;
-      std::vector<Map>::iterator it;
-      for(it = maps_vector.begin(); it != maps_vector.end(); ++it)
-      {
-        if(it->getMapFullName() == map_fullname)
-        {
-          return true;
-        }
-      }
-      return false;
-    }
+    res.success = true;
+    res.msg = "load_map service worked succesfully for: " + std::string(req.map_url);
+    return true;
+  }
 
+  bool dumpMapCallback(multimap_server_msgs::LoadMap::Request& req, multimap_server_msgs::LoadMap::Response& res)
+  {
+    // TODO
+    return true;
+  }
+
+  bool isMapAlreadyLoaded(std::string ns, std::string map_name)
+  {
+    std::string map_fullname = ns + "/" + map_name;
+    std::vector<Map>::iterator it;
+    for (it = maps_vector.begin(); it != maps_vector.end(); ++it)
+    {
+      if (it->getMapFullName() == map_fullname)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
 };
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
   ros::init(argc, argv, "multimap_server", ros::init_options::AnonymousName);
-  if(argc != 2)
+  if (argc != 2)
   {
     ROS_ERROR("%s", USAGE);
     exit(-1);
@@ -349,7 +367,7 @@ int main(int argc, char **argv)
     MultimapServer ms(fname);
     ros::spin();
   }
-  catch(std::runtime_error& e)
+  catch (std::runtime_error& e)
   {
     ROS_ERROR("multimap_server exception: %s", e.what());
     return -1;
